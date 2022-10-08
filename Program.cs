436f1b777a7178ab6fce1c898d98734feb5b0f77
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-08 13:51:33 +0530

    added UC-1 option to switch case in main method

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CoreAndFunctionalProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs!");
+            Console.WriteLine("Program #1: Flip Coin");
+
+            Console.Write("Please select a program to run from options above: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    FlipCoin.CalculatePercentage();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid number from given options");
+                    break;
+            }
+        }
+    }
+}
