d0351f96e2fc0c1247b00a86c59a4d5b62f9ecaa
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-08 15:44:34 +0530

    added practice problem 10

---
+++ LargestNumberAmongThree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreAndFunctionalProgramming
+{
+    public static class LargestNumberAmongThree
+    {
+        public static void FindTheLargest()
+        {
+            Console.Write("Enter three numbers to find the largest number among them\nEnter first number: ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int numThree = Convert.ToInt32(Console.ReadLine());
+
+            if(numOne >= numTwo && numOne >= numThree)
+            {
+                Console.WriteLine("Largest number is: " + numOne);
+            }
+            else if (numTwo > numOne && numTwo > numThree)
+            {
+                Console.WriteLine("Largest number is: " + numTwo);
+            }
+            else
+            {
+                Console.WriteLine("Largest number is: " + numThree);
+            }
+        }
+    }
+}
