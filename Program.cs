d313d66cfbbf5ca71ef598b01c9e76ccabfa545d
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-08 14:02:18 +0530

    added PP-2 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Welcome to Basic Core Programs!");
             Console.WriteLine("Program #1: Flip Coin");
+            Console.WriteLine("Program #2: Leap Year");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -14,6 +15,9 @@
             {
                 case 1:
                     FlipCoin.CalculatePercentage();
+                    break;
+                case 2:
+                    LeapYear.CheckLeapYear();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
