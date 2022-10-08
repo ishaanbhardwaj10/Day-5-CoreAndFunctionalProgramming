daae42305e775bf3c9730f5394130d9f6ba439b6
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-08 14:51:29 +0530

    added PP-4 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("Program #1: Flip Coin");
             Console.WriteLine("Program #2: Leap Year");
             Console.WriteLine("Program #3: Power of 2");
+            Console.WriteLine("Program #4: Harmonic Number");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -22,6 +23,9 @@
                     break;
                 case 3:
                     PowerOfTwo.PrintTable();
+                    break;
+                case 4:
+                    HarmonicNumber.PrintSeries();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
